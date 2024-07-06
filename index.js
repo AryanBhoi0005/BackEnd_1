33cf4d7f4edb5d79b495c9e5ca15a74e63a49d9b
AryanBhoi0005 <aryanbhoi1962@gmail.com>
2024-07-06 11:56:13 +0530

    add

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config() //Copied from dotenv website
+const express = require('express')
+const app = express()
+// const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("I am Twitter say Hi")
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<H1>Logged In</H1>')
+})
+ 
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
